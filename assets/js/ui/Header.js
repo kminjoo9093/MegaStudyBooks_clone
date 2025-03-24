52c7af75c7827c067d26de507da66793f6cb403e
kminjoo9093 <kminjoo9093@naver.com>
2025-03-24 17:44:44 +0900

    favicon 추가

--- assets/js/ui/Header.js
+++ assets/js/ui/Header.js
@@ -1,7 +1,6 @@
 export const HeaderHeight = ()=>{
   function setMarginTop(){
     const headerHeight = Math.ceil(document.querySelector('.header-inner').offsetHeight);
-    console.log(headerHeight)
     const container = document.querySelector('#container');
   
     container.style.setProperty('--headerHeight', `${headerHeight/10}rem`);
